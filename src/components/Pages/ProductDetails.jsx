cd0e051730da22bdc159a36363ca9b9243b313be
lezyCoder <iamvijaysingh23@gmail.com>
2026-01-12 10:09:57 +0530

    Dispatching the action

--- src/components/Pages/ProductDetails.jsx
+++ src/components/Pages/ProductDetails.jsx
@@ -1,15 +1,19 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { addToCart } from "../Slices/cartSlice";
 
 const ProductDetails = () => {
   // taking id from the  url (return string)
   const { id } = useParams();
 
+  // dispatch for the action
+  const dispatch = useDispatch();
+
+
   // taking product state from the store
   const { items, isloading, error } = useSelector((state) => state.product);
 
   // console.log("productDetails", items);
-
   const product = items.find((item) => Number(id) === item.id);
 
   // const product = items[id];
@@ -18,30 +22,46 @@ const ProductDetails = () => {
   // console.log("product:", product);
 
   return (
-    <div className="border product-details-wrapper grid grid-cols-1 md:grid-cols-2 gap-6 p-6 min-h-[80vh]">
-      {/* Left - Product Image */}
-      <div className="left flex items-center justify-center p-4 bg-yellow-50">
-        <img
-          src={product.thumbnail}
-          alt={product.title}
-          className="max-w-full max-h-[600px] object-contain"
-        />
-      </div>
+    <>
+      {isloading && (
+        <p className="text-purple-300 text-center text-2xl">
+          Loading product...
+        </p>
+      )}
 
-      {/* Right - Product Info */}
-      <div className="right  p-4 flex flex-col justify-between">
-        <div className="product-info space-y-4">
-          <h1 className="text-2xl font-semibold">{product.title}</h1>
-          <p className="text-xl font-bold text-purple-600">${product.price}</p>
-          <p className="text-gray-600">{product.description}</p>
-          <p className="text-lg font-thin text-purple-600">Rating : {product.rating}</p>
+      {product && (
+        <div className="border border-gray-700 rounded product-details-wrapper grid grid-cols-1 md:grid-cols-2 gap-6 p-6 min-h-[80vh]">
+          {/* Left - Product Image */}
+          <div className="left flex items-center justify-center p-4 bg-yellow-50">
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+              className="max-w-full max-h-[600px] object-contain"
+            />
+          </div>
+
+          {/* Right - Product Info */}
+          <div className="right p-4 flex flex-col justify-between">
+            <div className="product-info space-y-4">
+              <h1 className="text-2xl font-semibold">{product.title}</h1>
+              <p className="text-xl font-bold text-purple-600">
+                ${product.price}
+              </p>
+              <p className="text-gray-600">{product.description}</p>
+              <p className="text-lg font-thin text-purple-600">
+                Rating : {product.rating}
+              </p>
+            </div>
+
+            <button
+              className="mt-6 px-4 py-2 bg-purple-500 text-white rounded-md hover:bg-purple-600 cursor-pointer transition w-full"
+              onClick={() => dispatch(addToCart(product))}>
+              Add to cart
+            </button>
+          </div>
         </div>
-
-        <button className="mt-6 px-4 py-2 bg-purple-500 text-white rounded-md hover:bg-purple-600 transition w-full ">
-          Add to cart
-        </button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
