550817f4493792296a85b1e565e2837e3d787ba7
lezyCoder <iamvijaysingh23@gmail.com>
2026-01-16 00:15:53 +0530

    Update Checkout component: change heading to "Coming soon ..." and apply styling for better visibility

--- src/components/Pages/Checkout.jsx
+++ src/components/Pages/Checkout.jsx
@@ -1,13 +1,9 @@
-
-
 const Checkout = () => {
-
-    return (
-        <>
-
-            <h1>Checkout</h1>
-        </>
-    )
-}
+  return (
+    <>
+        <h1 className="text-5xl font-bold text-purple-500">Coming soon ...</h1>
+    </>
+  );
+};
 
 export default Checkout;
